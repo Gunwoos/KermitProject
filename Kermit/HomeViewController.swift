60199f0e9eede0a4c15971b97d3486d7e4db6555
Gun woo Lim <lkwyoyo@gmail.com>
2018-07-20 11:01:18 +0900

    first commit

---
+++ Kermit/HomeViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Kermit
+//
+//  Created by 임건우 on 2018. 7. 20..
+//  Copyright © 2018년 lims. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Kermit/LoadingViewController.swift
@@ -0,0 +1,35 @@
+//
+//  LoadingViewController.swift
+//  Kermit
+//
+//  Created by 임건우 on 2018. 7. 20..
+//  Copyright © 2018년 lims. All rights reserved.
+//
+
+import UIKit
+
+class LoadingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
